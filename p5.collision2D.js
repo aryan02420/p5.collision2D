031e1a0da481afbfdf51dd667222d6638c53f188
aryan02420 <aryan02420@gmail.com>
2021-03-12 15:30:24 +0530

    added box box collisions

--- p5.collision2D.js
+++ p5.collision2D.js
@@ -4,126 +4,28 @@ function Collision2D(p) {
   this.objects = []
   let THIS = this
 
-  this.collisionPoint = class {
-    type = 'POINT'
-    constructor(...args) {
-      if (args.length === 1 && args[0] instanceof p5.Vector) {
-        this.position = args[0]
-      } else if (args.length === 2) {
-        this.position = THIS.sketch.createVector(args[0], args[1])
-      } else {
-        throw 'unknown signature in constructor collisonPoint'
-      }
-      THIS.objects.push(this)
-    }
-    get pos() {
-      return this.position
-    }
-    set pos(vec) {
-      if (vec instanceof p5.Vector) {
-        this.position = vec
-        return
-      }
-      throw 'collisionPoint.pos accepts only p5.Vector'
-    }
-    draw = function() {
-      THIS.sketch.circle(this.position.x, this.position.y, 2)
-      THIS.sketch.circle(this.position.x, this.position.y, 5)
-    }
-  }
-  
-  this.collisionBox = class {
-    type = 'BOX'
-    constructor(...args) {
-      if (args.length === 2 && args[0] instanceof p5.Vector && args[1] instanceof p5.Vector) {
-        this.from = args[0]
-        this.to = args[1]
-      } else if (args.length === 4) {
-        this.from = THIS.sketch.createVector(args[0], args[1])
-        this.to = THIS.sketch.createVector(args[2], args[3])
-      } else {
-        throw 'unknown signature in constructor collisonBox'
-      }
-      THIS.objects.push(this)
-    }
-    get center() {
-      return p5.Vector.add(this.from, this.to).mult(0.5)
-    }
-    get width() {
-      return THIS.sketch.abs(this.from.x - this.to.x)
-    }
-    get height() {
-      return THIS.sketch.abs(this.from.y - this.to.y)
-    }
-    get size() {
-      return THIS.sketch.createVector(this.width, this.height)
-    }
-    set center(vec) {
-      if (vec instanceof p5.Vector) {
-        let currentpos = this.center
-        let displacement = vec.sub(currentpos)
-        this.from.add(displacement)
-        this.to.add(displacement)
-        return
-      }
-      throw 'collisionBox.center accepts only p5.Vector'
-    }
-    set width(w) {
-      this.from = THIS.sketch.createVector(this.center.x - w/2, this.from.y)
-      this.to = THIS.sketch.createVector(this.center.x + w/2, this.to.y)
-    }
-    set height(h) {
-      this.from = THIS.sketch.createVector(this.from.x, this.center.y - h/2)
-      this.to = THIS.sketch.createVector(this.to.x , this.center.y + h/2)
-    }
-    set size(vec) {
-      if (vec instanceof p5.Vector) {
-        this.width = vec.x
-        this.height = vec.y
-        return
-      }
-      throw 'collisionBox.size accepts only p5.Vector'
-    }
-    draw = function() {
-      THIS.sketch.rectMode(CORNERS);
-      THIS.sketch.rect(this.from.x, this.from.y, this.to.x, this.to.y)
-    }
-  }
-
   this.collisionPrimitive = function (type, ...args) {
     switch (type.toString().toUpperCase()) {
       case 'POINT':
-        return new THIS.collisionPoint(...args)
+        return new THIS._collisionPoint(THIS, ...args)
       case 'BOX':
-        return new THIS.collisionBox(...args)
+        return new THIS._collisionBox(THIS, ...args)
       default:
         throw 'unknown primitive type'
     }
-  }
-
-  this.collidingPointPoint = function(point1, point2, margin) {
-    margin ||= 2
-    return (point1.position.dist(point2.position) <= margin)
-  }
-  
-  this.collidingPointBox = function(point, box, margin) {
-    return (point.position.x >= box.from.x &&
-      point.position.x <= box.to.x &&
-      point.position.y >= box.from.y &&
-      point.position.y <= box.to.y)
   }
 
   this.colliding = function(obj1, obj2, margin) {
     let typeOfCollision = obj1.type + obj2.type
     switch (typeOfCollision) {
       case 'POINTPOINT':
-        return THIS.collidingPointPoint(obj1, obj2, margin)
-      case 'POINTLINE':
-        return THIS.collidingPointLine(obj1, obj2, margin)
+        return THIS._collidingPointPoint(obj1, obj2, margin)
       case 'POINTBOX':
-        return THIS.collidingPointBox(obj1, obj2, margin)
+        return THIS._collidingPointBox(obj1, obj2, margin)
       case 'BOXPOINT':
-        return THIS.collidingPointBox(obj2, obj1, margin)
+        return THIS._collidingPointBox(obj2, obj1, margin)
+      case 'BOXBOX':
+        return THIS._collidingBoxBox(obj2, obj1, margin)
       default:
         throw 'unknown collision type'
   
@@ -142,4 +44,155 @@ function Collision2D(p) {
   }
 }
 
+Collision2D.prototype._collisionPoint = class {
 
+  type = 'POINT'
+
+  constructor(parent, ...args) {
+    this.parent = parent
+    if (args.length === 1 && args[0] instanceof p5.Vector) {
+      this._position = args[0]
+    } else if (args.length === 2) {
+      this._position = this.parent.sketch.createVector(args[0], args[1])
+    } else {
+      throw 'unknown signature in constructor collisonPoint'
+    }
+    this.parent.objects.push(this)
+  }
+
+  get pos() {
+    return this._position
+  }  
+  get x() {
+    return this._position.x
+  }  
+  get y() {
+    return this._position.y
+  }
+
+  set pos(vec) {
+    if (vec instanceof p5.Vector) {
+      this._position = vec
+      return
+    }
+    throw 'collisionPoint.pos accepts only p5.Vector'
+  }  
+  set x(x) {
+    this._position.x = x
+  }  
+  set y(y) {
+    this._position.y = y
+  }
+
+  draw = function() {
+    this.parent.sketch.circle(this.pos.x, this.pos.y, 2)
+    this.parent.sketch.circle(this.pos.x, this.pos.y, 5)
+  }
+}
+
+Collision2D.prototype._collisionBox = class {
+
+  type = 'BOX'
+
+  constructor(parent, ...args) {
+    this.parent = parent
+    if (args.length === 2 && args[0] instanceof p5.Vector && args[1] instanceof p5.Vector) {
+      this._center = args[0]
+      this._size = args[1]
+    } else if (args.length === 4) {
+      this._center = this.parent.sketch.createVector(args[0], args[1])
+      this._size = this.parent.sketch.createVector(args[2], args[3])
+    } else {
+      throw 'unknown signature in constructor collisonBox'
+    }
+    this.parent.objects.push(this)
+  }
+
+  get center() {
+    return this._center
+  }
+  get size() {
+    return this._size
+  }
+  get cx() {
+    return this._center.x
+  }
+  get cy() {
+    return this._size
+  }
+  get width() {
+    return this._center.y
+  }
+  get height() {
+    return this._center.y
+  }
+  get _xmin() {
+    return this._center.x - this._size.x/2
+  }
+  get _xmax() {
+    return this._center.x + this._size.x/2
+  }
+  get _ymin() {
+    return this._center.y - this._size.y/2
+  }
+  get _ymax() {
+    return this._center.y + this._size.y/2
+  }
+
+  set center(vec) {
+    if (vec instanceof p5.Vector) {
+      this._center = vec
+      return
+    }
+    throw 'collisionBox.center accepts only p5.Vector'
+  }
+  set size(vec) {
+    if (vec instanceof p5.Vector) {
+      this._size = vec
+      return
+    }
+    throw 'collisionBox.size accepts only p5.Vector'
+  }
+  set cx(x) {
+    this._center.x = x
+  }
+  set cy(y) {
+    this._center.y = y
+  }
+  set width(w) {
+    this._size.x = w
+  }
+  set height(h) {
+    this._size.y = h
+  }
+
+  draw = function() {
+    this.parent.sketch.rectMode(CENTER);
+    this.parent.sketch.rect(this.center.x, this.center.y, this.size.x, this.size.y)
+  }
+
+}
+
+
+Collision2D.prototype._collidingPointPoint = function(point1, point2, margin) {
+  margin = margin || 2
+  return (point1.pos.dist(point2.pos) <= margin)
+}
+
+Collision2D.prototype._collidingPointBox = function(point, box) {
+  return (
+    point.pos.x >= box._xmin &&
+    point.pos.x <= box._xmax &&
+    point.pos.y >= box._ymin &&
+    point.pos.y <= box._ymax
+  )
+}
+
+Collision2D.prototype._collidingBoxBox = function(box1, box2) {
+  return (
+    box1._xmax >= box2._xmin &&
+    box1._xmin <= box2._xmax &&
+    box1._ymax >= box2._ymin &&
+    box1._ymin <= box2._ymax
+  )
+}
--- readme.md
+++ readme.md
@@ -1,6 +1,7 @@
 # p5.collision2D
 
 rewrite of [p5.collide2D](https://github.com/bmoren/p5.collide2D)
+this project is a work in progress, the api is always changing
 
 -----
 
@@ -8,7 +9,7 @@ rewrite of [p5.collide2D](https://github.com/bmoren/p5.collide2D)
 
  - :green_circle:   point
  - :red_circle:     line
- - :green_circle:  box
+ - :green_circle:   box
  - :red_circle:     circle
  - :red_circle:     ellipse
  - :red_circle:     arc
@@ -21,9 +22,9 @@ rewrite of [p5.collide2D](https://github.com/bmoren/p5.collide2D)
 
 |               | point         | line          | box           | circle        | ellipse       | arc           | polygon       | triangle      | capsule       |
 | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: | :-----------: |
-| point         |:green_circle: |:red_circle:   |:green_circle:|:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
+| point         |:green_circle: |:red_circle:   |:green_circle: |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
 | line          |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
-| box           |:green_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
+| box           |:green_circle: |:red_circle:   |:green_circle: |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
 | circle        |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
 | ellipse       |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
 | arc           |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |:red_circle:   |
@@ -37,61 +38,46 @@ ____
 
 ##### Instanciate the library
 
-```ts
+```
 let c2d = new Collision2D()
 ```
 ##### Creating Primitives
 
 ###### Point
 
-```ts
-let point1 = new c2d.collisionPoint(x: float, y: float)
-let point2 = new c2d.collisionPoint(position: p5.Vector)
+```
+let point1 = c2d.collisionPoint('POINT', x: number, y: number)
+let point2 = c2d.collisionPoint('POINT', position: p5.Vector)
 
 // properties
 collisionPoint.pos: p5.Vector
+collisionPoint.x: number
+collisionPoint.y: number
 ```
 
 ###### Box
 
-```ts
-let box1 = new c2d.collisionBox(x1: float, y1: float, x2: float, y2: float)
-let box2 = new c2d.collisionBox(position1: p5.Vector, position2: p5.Vector)
+```
+let box1 = c2d.collisionBox('BOX', x1: float, y1: float, x2: float, y2: float)
+let box2 = c2d.collisionBox('BOX', position1: p5.Vector, position2: p5.Vector)
 
 // properties
 collisionBox.center: p5.Vector
+collisionBox.size: p5.Vector
 collisionBox.width: number
 collisionBox.height: number
-collisionBox.size: p5.Vector
-```
-
-###### Shorthand
-
-```ts
-let obj = new c2d.collisionPrimitive(type: string, [...args])
-// where type is one of 
-// POINT, BOX
-// args will be passed to the correct constructor
+collisionBox.cx: number
+collisionBox.cy: number
 ```
 
 ##### Checking Collisions
 
-###### Point-Point
-
-```ts
-c2d.collidingPointPoint(p1: collisionPoint, p2: collisionPoint, margin: float) -> boolean
 ```
-
-###### Shorthand
-
-```ts
-c2d.colliding(o1: any, o2: any, margin: float) -> boolean
-// will find the type of collision
-// and call the appropriate method
+c2d.colliding(obj1: any, obj2: any, [margin: float]): boolean
 ```
 
 ##### Drawing Hitboxes
 
-```ts
+```
 c2d.drawCollisionOverlays()
 ```
